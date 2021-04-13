4e965951d4bf1ed3a3dc570c9850b570f7f576a5
renatodamas <damas.renato@gmail.com>
2021-04-12 21:56:10 -0300

    Configurado ALLOWES_HOSTS

--- pypro/settings.py
+++ pypro/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from decouple import Csv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_5!o-q6c7tt1yw%_4$we5op)bw0re!-_epgf-#lplu7k$3=l@j'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)  # lib 'python-decouple'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
