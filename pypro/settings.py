93cc540e344366ef3ff2be9c90d70e8dd566f0d4
renatodamas <damas.renato@gmail.com>
2022-03-24 14:21:01 -0300

    flake8 config file

--- pypro/settings.py
+++ pypro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fylc5c%3@j526$jd4c2xkzy#wjre!(fetnz-$whc5pd533=qj+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
