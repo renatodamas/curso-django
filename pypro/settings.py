028d859e91d9ff399dd84e158acbc10764da9678
renatodamas <damas.renato@gmail.com>
2022-05-31 17:22:22 -0300

    updating libs

--- pypro/settings.py
+++ pypro/settings.py
@@ -132,8 +132,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 COLLECTFAST_ENABLE = False
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-COLLECTFAST_STRATEGY = 'collectfast.strategies.boto3.Boto3Strategy'
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
@@ -154,7 +152,9 @@ if AWS_ACCESS_KEY_ID:
 
     # Static Assets
     # ===================================================================
-    STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    COLLECTFAST_STRATEGY = 'collectfast.strategies.boto3.Boto3Strategy'
+    # STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
     STATIC_S3_PATH = 'static'
     STATIC_ROOT = f'/{STATIC_S3_PATH}/'
     STATIC_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}/'
