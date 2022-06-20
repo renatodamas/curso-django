bcf3d2772eb2a3c81065de0a0a986f98f3c5881b
renatodamas <damas.renato@gmail.com>
2022-06-20 15:29:27 -0300

    Incorporado video de motivação

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -2,6 +2,8 @@ import pytest
 from django.urls import reverse
 from django.test import Client
 
+from pypro.django_assertions import assert_contains
+
 
 @pytest.fixture
 def resp(client: Client):
@@ -10,3 +12,11 @@ def resp(client: Client):
 
 def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_video_title(resp):
+    assert_contains(resp, '<h1>Video Aperitivo: Motivação</h1>')
+
+
+def test_video_content(resp):
+    assert_contains(resp, '<iframe src="https://www.youtube.com/embed/2aYplgJrPDU"')
